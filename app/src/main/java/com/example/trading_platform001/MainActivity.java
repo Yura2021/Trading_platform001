a47ff933a88ee568e58d9ee4d5e54100c5ecd54e
Yura2021 <79528600+Yura2021@users.noreply.github.com>
2022-08-09 17:01:30 +0300

    update bar v5

--- app/src/main/java/com/example/trading_platform001/MainActivity.java
+++ app/src/main/java/com/example/trading_platform001/MainActivity.java
@@ -1,44 +1,50 @@
 package com.example.trading_platform001;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.content.res.AppCompatResources;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.res.ResourcesCompat;
-import androidx.core.graphics.drawable.DrawableCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
-import android.graphics.Color;
-import android.graphics.PorterDuff;
-import android.graphics.PorterDuffColorFilter;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ImageButton;
+import android.widget.SearchView;
 
-import java.util.Objects;
+import com.example.trading_platform001.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    ImageButton yourButton;
+    ActivityMainBinding binding;
+    ViewDataBinding activityMainBinding;
+
+    List<String> arrayList= new ArrayList<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
          //yourButton = findViewById(R.id.button1);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new HomeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+
+            switch (item.getItemId()){
+                case R.id.home:
+                    replaceFragment(new HomeFragment());
+                    break;
+            }
+            return true;
+        });
 
     }
 
-    public void onClickButton(View v) {
-/*
-        Drawable mDrawable = ContextCompat.getDrawable(this, R.drawable.image_test).mutate();
-        mDrawable.setColorFilter(new PorterDuffColorFilter(R.color.green, PorterDuff.Mode.SRC_IN));
-        yourButton.setCompoundDrawables(null, mDrawable, null, null);
-
-        Drawable mDrawable = ContextCompat.getDrawable(yourButton.getContext(),R.drawable.image_test);
-        mDrawable.setColorFilter(new
-                PorterDuffColorFilter(Color.GREEN,PorterDuff.Mode.SRC_IN));
-        yourButton.setCompoundDrawables(null, mDrawable, null, null);
-         */
-        //yourButton.setColorFilter(Color.GREEN);
+    public void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.layout_view_fragment,fragment);
+        fragmentTransaction.commit();
     }
 
 }
