47ccfc61b117ebc50a02491df67803a74eb8bc3c
NazarKov <nazar.korniychuk@ukr.net>
2022-09-29 21:16:41 +0300

    create update catalog

--- app/src/main/java/com/example/trading_platform001/catalog_page/ChildrenCategoryFragment.java
+++ app/src/main/java/com/example/trading_platform001/catalog_page/ChildrenCategoryFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -13,6 +14,10 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 import com.example.trading_platform001.R;
+import com.example.trading_platform001.catalog_page.models.Category;
+import com.example.trading_platform001.models.LocalCategory;
+
+import java.util.ArrayList;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -28,6 +33,14 @@ public class ChildrenCategoryFragment extends Fragment {
     ImageView imageBackCategoriChildren;
     Bundle bundle;
     View view;
+    ArrayList<Category> categories;
+    ViewGroup viewGroup;
+    @BindView(R.id.layoutLeftCategoryChildren)
+    LinearLayout layoutLeftCategory;
+    @BindView(R.id.layoutRightCategoryChildren)
+    LinearLayout layoutRightCategory;
+    int parent_id;
+
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
@@ -52,10 +65,38 @@ public class ChildrenCategoryFragment extends Fragment {
         bundle = getArguments();
         if (bundle != null) {
             NameBackCategoriChildren.setText(bundle.getString("NameParenCategory"));
+            parent_id=bundle.getInt("tagParenCategory");
         }
         imageBackCategoriChildren.setOnClickListener(v->onClick());
+        categories= LocalCategory.getCategory();
+        ShowCategoryItem(inflater);
 
         return view;
+    }
+
+
+
+    public void ShowCategoryItem(LayoutInflater inflater)
+    {
+        for(int i = 0 ; i<categories.size();i++)
+        {
+            View row = inflater.inflate(R.layout.category_item_template,viewGroup, false);
+            TextView name = row.findViewById(R.id.NameCategoryItemTemplate);
+            ImageView image = row.findViewById(R.id.ImageCategoryItemTemplate);
+
+            //row.setOnClickListener(k->onClick(row));
+
+            if(categories.get(i).getParent_id()==parent_id) {
+                image.setImageResource(R.drawable.ic_launcher_background);
+                name.setText(categories.get(i).getName());
+                if (i == 2 || i % 2 == 0) {
+                    layoutLeftCategory.addView(row);
+                } else {
+                    layoutRightCategory.addView(row);
+                }
+            }
+        }
+
     }
     public  void onClick()
     {
